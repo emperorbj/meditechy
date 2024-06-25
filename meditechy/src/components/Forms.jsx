602a49a2c1ae125438ea418b2a275ec682c56bdc
emperorbj <cwaku96@gmail.com>
2024-06-25 12:51:49 +0100

    added contact page with styles

--- meditechy/src/components/Forms.jsx
+++ meditechy/src/components/Forms.jsx
@@ -11,7 +11,7 @@ const Forms = () => {
     const [agreed, setAgreed] = useState(false)
 
     return (
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 w-[50%]">
             <div
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true"
@@ -25,7 +25,7 @@ const Forms = () => {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
                     Aute magna irure deserunt veniam aliqua magna enim voluptate.
                 </p>
@@ -102,9 +102,13 @@ const Forms = () => {
                                     name="country"
                                     className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                                 >
-                                    <option>US</option>
-                                    <option>CA</option>
+                                    <option>NG</option>
+                                    <option>GH</option>
                                     <option>EU</option>
+                                    <option>US</option>
+                                    <option>CH</option>
+                                    <option>UAE</option>
+                                    <option>JA</option>
                                 </select>
                                 <ChevronDownIcon
                                     className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
@@ -156,7 +160,7 @@ const Forms = () => {
                         </div>
                         <Label className="text-sm leading-6 text-gray-600">
                             By selecting this, you agree to our{' '}
-                            <a href="#" className="font-semibold text-indigo-600">
+                            <a href="#" className="font-semibold text-blue-600">
                                 privacy&nbsp;policy
                             </a>
                             .
@@ -166,7 +170,7 @@ const Forms = () => {
                 <div className="mt-10">
                     <button
                         type="submit"
-                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="block w-full rounded-md bg-blue-600 px-3.5 py-3.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                     >
                         Lets talk
                     </button>
